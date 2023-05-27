d8e651b76436b3c06018aa18c382f26ce21a8350
Amit Korgaonkar <rginslemearanddgoaprojectstudent4.in@siemens.com>
2023-05-27 09:05:47 +0530

    Added cpp programs

---
+++ FromHackerRank/CPP_Programs/String_split.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <string.h>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    // char str3[10];
+    string str,str1,str2;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin >> str;
+        for(int j=0;j<str.length();j++)
+        {
+            if(j % 2 == 0)
+            {
+                str1 += str[j];
+            }
+            else{
+                str2 +=str[j];
+            }
+        }
+        cout << str1 << " " << str2 <<endl;
+        memset(str3,0,sizeof(str3));
+        // str1.clear();
+        str2.clear();
+    }    
+    
+    return 0;
+}
