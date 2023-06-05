5436570640aad3373aa17c58237e9cbd224bc26b
Amit Korgaonkar <rginslemearanddgoaprojectstudent4.in@siemens.com>
2023-06-05 16:28:14 +0530

    Initialization list in cpp

--- C++_Programs/Initializer_List.cpp
+++ C++_Programs/Initializer_List.cpp
@@ -1,43 +1,25 @@
-// #include <iostream>
-// #include <initializer_list>
-
-// using namespace std;
-
-// int main() {
-//     initializer_list<initializer_list<double>> il = {
-//         {1.1, 2.2, 3.3},
-//         {4.4, 5.5, 6.6},
-//         {7.7, 8.8, 9.9}
-//     };
-
-//     for (const auto& innerList : il) {
-//         for (const auto& value : innerList) {
-//             cout << value << ' ';
-//         }
-//         cout << '\n';
-//     }
-
-//     return 0;
-// }
-
-
 #include <iostream>
 #include <initializer_list>
 
 using namespace std;
 
-void printNumbers(initializer_list<int> numbers) {
+void printNumbers(initializer_list<initializer_list<int>> numbers) {
     cout << "Numbers: ";
-    for(auto number : numbers) 
+    for(auto num : numbers)
     {
-        cout << number << " ";
+        // cout << num <<endl;
+        for(auto number : num) 
+        {
+            // ostream << initializer_list<int>;
+            cout << number << " ";
+        }
     }
-    cout << endl;
+        cout << endl;
 }
 
 int main() {
-    printNumbers({1, 2, 3, 4, 5});
-    printNumbers({10, 20, 30});
+    printNumbers({{1, 2, 3, 4, 5}});
+    // printNumbers({10, 20, 30});
 
     return 0;
 }
