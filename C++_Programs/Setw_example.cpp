fa903b1b73b239f171dba5f0dec7af07bc68324a
Amit Korgaonkar <rginslemearanddgoaprojectstudent4.in@siemens.com>
2023-06-20 16:45:56 +0530

    Added Error Window file

--- C++_Programs/Setw_example.cpp
+++ C++_Programs/Setw_example.cpp
@@ -14,7 +14,14 @@ int main()
 	cout << "Before setting the width: \n" << num << endl;
 
 	// Using setw()
-	cout << "Setting the width"	<< " using setw to 5: \n"<< setw(5)<<num;
+	cout << "Setting the width"	<< " using setw to 5: \n"<< setw(5)<<num<<endl;
+
+
+    //  int num = 97;
+     std::cout << "  " << std::setw(5) << num
+     << "  " << std::setw(14) << num
+     << "  " << std::setw(14) << num
+     << "  " << std::setw(14) << num << "\n";
 
 	return 0;
 }
