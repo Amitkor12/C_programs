d0b167d7669bb6f426b6853279c2df64676923f9
Amit Korgaonkar <rginslemearanddgoaprojectstudent4.in@siemens.com>
2023-06-20 16:46:36 +0530

    trying width modification in c for printf

--- C_Programs/FromBooks/Learn_programing_with_C-Book-/Printf.c
+++ C_Programs/FromBooks/Learn_programing_with_C-Book-/Printf.c
@@ -20,15 +20,23 @@ void main()
 
     #endif
 
-    #if 1
+    #if 0
         /*Format Specifiaction while using printf */
         
         int num = 97;
         printf("%d\n",num);
         printf("%d04\n",num);
-        printf("%04d\n",num);       //leading zeroes
-        printf("%4d\n",num);           //leading spaces
-        printf("%-4d\n",num);       //trailling spaces 
+        printf("leading zeroes: %04d\n",num);       //leading zeroes
+        printf("leading spaces: %5d\n",num);           //leading spaces
+        printf("trailling spaces: %-4d\n",num);       //trailling spaces 
 
+    #endif
+
+    #if 1
+      double num = 97;
+      printf("  %5lf    %14lf   %14lf   %14lf",num,num,num,num);              //std::cout << "  " << std::setw(5) << it
+    //   printf("  %14lf",num);        //<< "  " << std::setw(14) << *lambda
+    //   printf("  %14lf",num);        //<< "  " << std::setw(14) << val_dif
+    //   printf("  %14lf\n",num);       //<< "  " << std::setw(14) << sin_y1y2 << "\n";
     #endif
 }
