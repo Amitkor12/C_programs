400f2a21e70b29a48a57d3253961d030cd6dff6c
Amit Korgaonkar <rginslemearanddgoaprojectstudent4.in@siemens.com>
2023-06-22 16:55:19 +0530

    some pointer exercise

--- C_Programs/pointer_exercise.c
+++ C_Programs/pointer_exercise.c
@@ -1,13 +1,42 @@
 #include<stdio.h>
 
+typedef struct complex
+{
+    double num;
+}complex;
+
+double real(complex* cmp)
+{
+    return cmp->num;
+}
+
+typedef struct roots
+{
+    complex* cmp1;
+    double num1;
+}roots;
+
+complex* pole(roots* rt,int oderno)
+{
+    return &rt->cmp1[oderno];
+}
+
 void main()
 {
-    char a = 100;
-    printf("adress of a:\t%p",&a);
-    char *ptr = &a;
-    printf("\nValue of pointer:\t%d",*ptr);
-    *ptr = 120;
-    printf("\nValue of a:\t%d",a);
+    int a = 100;
+    complex cmp;
+    cmp.num = 5;
+    roots rt;
+    rt.num1 = 6;
+    // real(&cmp);
+    printf("%lf\n",real(&cmp));
+    
+    // printf("adress of a:\t%p",&a);
+    // char* ptr = &a;
+    // printf("\nValue of pointer:\t%d",*ptr);
+    // *ptr = 120;
+    // printf("\nValue of a:\t%d\n",a);
+    // printf("%d\n",(&ptr));
 }
 
 // #include<stdio.h>
