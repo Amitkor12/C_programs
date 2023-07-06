159b0cb7108878737044702d052fb568e55e3f25
Amit Korgaonkar <rginslemearanddgoaprojectstudent4.in@siemens.com>
2023-07-06 16:24:26 +0530

    using typedef concept

--- C_Programs/structures/structures_program.c
+++ C_Programs/structures/structures_program.c
@@ -1,109 +1,21 @@
 // finding percentage for single student
 #include <stdio.h>
-struct student{
+typedef struct student{                                //create struct using typedef
     char name[30];
     int Roll_no;
     float P_marks,Ch_marks,M_marks;
     float percentage;
-};
+}tstudent;                                          //name for typedef 
 
 void main()
 {
-    struct student info;
+    tstudent sturct_info;                   //using name of typedef and creating object for structure
     printf("enter student name:");
-    scanf("%s",info.name);
+    scanf("%s",sturct_info.name);           //through object of structure accessing the members of structure
     printf("enter roll_no:");
-    scanf("%d",&info.Roll_no);
+    scanf("%d",&sturct_info.Roll_no);
     printf("enter physics, chemistry and Maths marks:");
-    scanf("%f\t%f\t%f",&info.P_marks,&info.Ch_marks,&info.M_marks);
-    info.percentage = ((info.P_marks+info.Ch_marks+info.M_marks)/300)*100;
-    printf("the percentage is : %f",info.percentage);
+    scanf("%f\t%f\t%f",&sturct_info.P_marks,&sturct_info.Ch_marks,&sturct_info.M_marks);
+    sturct_info.percentage = ((sturct_info.P_marks+sturct_info.Ch_marks+sturct_info.M_marks)/300)*100;
+    printf("the percentage is : %f",sturct_info.percentage);
 }
-
-// sorting as per percentage
-// #include <stdio.h>
-// struct student{
-//     char name[30];
-//     int Roll_no;
-//     float P_marks,Ch_marks,M_marks;
-//     float percentage;
-// };
-
-// int main()
-// {
-// 	struct student info[4];
-//     int temp;
-//     for(int i=0;i<4;i++)
-//     {
-//         printf("enter student name:");
-//         scanf("%s",info[i].name);
-//         printf("enter roll_no:");
-//         scanf("%d",&info[i].Roll_no);
-//         printf("enter physic,chem and maths marks:");
-//         scanf("%f\t%f\t%f",&info[i].P_marks,&info[i].Ch_marks,&info[i].M_marks);
-//         info[i].percentage = ((info[i].P_marks+info[i].Ch_marks+info[i].M_marks)/300)*100;
-//         printf("the percentage is : %f\n",info[i].percentage);
-//     }
-//     for(int i=0;i<4-1;i++)
-//     {
-//         for(int j=0;j<4-1-i;j++)
-//         {
-//             if(info[j].percentage > info[j+1].percentage)
-//             {
-//                 temp = info[j].percentage;
-//                 info[j].percentage = info[j+1].percentage;
-//                 info[j+1].percentage = temp;
-//             }
-//         }
-//     }
-    
-//     for(int i=0;i<4;i++)
-//    {
-//         //printf("\n%f",info[i].percentage);
-//         printf("\nenter student name:%s",info[i].name);
-//         printf("\nenter roll_no:%d",info[i].Roll_no);
-//         printf("\nenter physic,chem and maths marks:%f\t%f\t%f",info[i].P_marks,info[i].Ch_marks,info[i].M_marks);
-//         printf("\nthe percentage is : %f\n",info[i].percentage);
-//    }
-
-//     return 0;
-// }
-
-// // program to print by variable and pointer
-// #include <stdio.h>
-// struct student{
-//     char name[30];
-//     int Roll_no;
-//     float P_marks,Ch_marks,M_marks;
-//     float percentage;
-// };
-
-// int main()
-// {
-//     struct student info;
-//     struct student *ptr = &info;
-    
-    
-//         printf("enter student name:");
-//         scanf("%s",info.name);
-//         printf("enter roll_no:");
-//         scanf("%d",&info.Roll_no);
-//         printf("enter physic,chem and maths marks:");
-//         scanf("%f\t%f\t%f",&info.P_marks,&info.Ch_marks,&info.M_marks);
-//         info.percentage = ((info.P_marks+info.Ch_marks+info.M_marks)/300)*100;
-//         printf("the percentage is : %f\n",info.percentage);
-        
-        
-//         printf("\n");
-//         printf("\nenter student name:%s",ptr->name);
-//         printf("\nenter roll_no:%d",ptr->Roll_no);
-//         printf("\nenter physic,chem and maths marks:%f\t%f\t%f",ptr->P_marks,ptr->Ch_marks,ptr->M_marks);
-//         printf("\nthe percentage is : %f\n",ptr->percentage);
-        
-//         printf("\n");
-//         printf("\nenter student name:%s",info.name);
-//         printf("\nenter roll_no:%d",info.Roll_no);
-//         printf("\nenter physic,chem and maths marks:%f\t%f\t%f",info.P_marks,info.Ch_marks,info.M_marks);
-//         printf("\nthe percentage is : %f\n",info.percentage);
-// return 0;
-// }
